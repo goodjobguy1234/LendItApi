3c68684ee2d59d60b5cc55413497add5ad355d41
goodjobguy1234 <goodjobguy1234@gmail.com>
2022-03-08 03:47:56 +0700

    fix

--- routes/transactions.js
+++ routes/transactions.js
@@ -230,8 +230,8 @@ router.patch('/:id', verify, (req,res) => {
         if(!doc) return res.notfound({message: "Transaction is not found"});
         if(err) return res.badreq({errors:err.errors, message: err.message});
 
-        Borrow.findById(doc.borrowID, (err, res) => {
-            Item.findByIdAndUpdate(res.itemID, {avaliable: true}, {new: true}).exec().then((value) => {
+        Borrow.findById(doc.borrowID, (err, resResult) => {
+            Item.findByIdAndUpdate(resResult.itemID, {avaliable: true}, {new: true}).exec().then((value) => {
                 if (!value) return res.internal({message: "cannot update item avaliable status"});
                 return res.success({message: "Trabsaction is complete, user have return the item"});
             }).catch((err) => {
